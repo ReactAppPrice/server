4739387fc81687ce38f3e81e4ab10fdac4b382a9
ReactAppPrice <123896748+ReactAppPrice@users.noreply.github.com>
2023-02-06 01:55:49 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,18 +8,41 @@ app.use(cors());
 app.use(express.json());
 
 let port = process.env.PORT || "8080";
-// mongoose
-//   .connect("mongodb://0.0.0.0:27017/priceComparison")
-//   .then(() => {
-//     console.log("Connected");
-//   })
-//   .catch((e) => {
-//     console.log("error");
-//   });
-app.get("/products", async (req, res) => {
-  // let data = await productList.find({});
-  // res.json(data);
-  res.json(products);
+mongoose
+  .connect(
+    "mongodb+srv://huzaifa:fireburning300@cluster0.xxwlzho.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected");
+  })
+  .catch((e) => {
+    console.log("error");
+  });
+//////////////////////////////
+
+//////////////////////////////
+
+let db = mongoose.connection;
+// Define a schema for the collection
+const productslists = new mongoose.Schema({
+  _id: String,
+  productName: String,
+  category: String,
+  image: String,
+  availableStore: Array,
+});
+
+// Compile the schema into a model
+const Item = mongoose.model("Item", productslists);
+// app.post("/products", async (req, res) => {
+//   let data = await Item.insertMany([...aaa]);
+//   res.json(data);
+//   res.json(products);
+// });
+
+app.get("/allProducts", async (req, res) => {
+  let data = await Item.find({});
+  res.json(data);
 });
 app.listen(port, () => {
   console.log("listning....");
