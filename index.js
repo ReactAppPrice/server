c490628d2b28b23628ea64cc24eb7dba87353a39
ReactAppPrice <123896748+ReactAppPrice@users.noreply.github.com>
2023-02-07 01:00:07 +0500

    new server upload

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const productList = require("./model");
+const { Expo } = require("expo-server-sdk");
 const products = require("./groceryItemsWithPrice.json");
 const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+// const expoSDK = Expo;
 let port = process.env.PORT || "8080";
+let expo = new Expo();
 mongoose
   .connect(
     "mongodb+srv://huzaifa:fireburning300@cluster0.xxwlzho.mongodb.net/?retryWrites=true&w=majority"
@@ -20,7 +22,6 @@ mongoose
   });
 
 let db = mongoose.connection;
-// Define a schema for the collection
 const productslists = new mongoose.Schema({
   _id: String,
   productName: String,
@@ -28,18 +29,13 @@ const productslists = new mongoose.Schema({
   image: String,
   availableStore: Array,
 });
-
-// Compile the schema into a model
 const Item = mongoose.model("Item", productslists);
 
-// for strore pushtoken ///////////
+// for store pushtoken start///////////
 const pushtoken = new mongoose.Schema({
   pushToken: String,
 });
-
-// token schema into a model
 const tokenPush = mongoose.model("tokens", pushtoken);
-
 app.post("/push-token", async (req, res) => {
   const token = new tokenPush({ pushToken: req.body.pushtoken });
   token
@@ -51,7 +47,54 @@ app.post("/push-token", async (req, res) => {
       res.json({ success: false });
     });
 });
+// for store pushtoken END///////////
 
+///////////////// FOR SHOW NOTIFICATION START ////////////////
+const notificationSchema = new mongoose.Schema({
+  message: String,
+});
+const Notification = mongoose.model("Notification", notificationSchema);
+app.post("/send-notification", async (req, res) => {
+  // if you want to add a notifications in your data base then enable the below two lines///////
+  // const notificationDocument = new Notification(req.body);
+  // await notificationDocument.save();
+
+  // Get all the push tokens from the database
+  const pushTokens = await tokenPush.find({});
+
+  // Get a list of all valid push tokens
+  const validPushTokens = [];
+  for (let pushToken of pushTokens) {
+    if (Expo.isExpoPushToken(pushToken.pushToken)) {
+      validPushTokens.push(pushToken.pushToken);
+    }
+  }
+  console.log(validPushTokens);
+
+  // Divide the valid push tokens into chunks of 100
+  const chunks = expo.chunkPushNotifications(
+    validPushTokens.map((pushToken) => ({
+      to: pushToken,
+      sound: "default",
+      body: "You have a new notification...",
+      data: req.body,
+    }))
+  );
+
+  // Send push notifications in chunks
+  for (let chunk of chunks) {
+    try {
+      const receipts = await expo.sendPushNotificationsAsync(chunk);
+      console.log(receipts);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  res.send({ success: true });
+});
+
+///////////////// FOR SHOW NOTIFICATION END ////////////////
 
 // if you delete alla the items then just open this code and call api
 // app.post("/products", async (req, res) => {
