f7fa791d11aa2089e2401cbcb72cc81ddc7c84da
ReactAppPrice <123896748+ReactAppPrice@users.noreply.github.com>
2023-02-08 05:10:16 +0500

    coupons api has been created with push notify

--- index.js
+++ index.js
@@ -112,18 +112,73 @@ app.get("/showNotifications", async (req, res) => {
   res.send(allNotifications);
 });
 ////////////////////////////////// GET ALL NOTIFICATIONS END /////////////////////////////////////
-
-// if you delete alla the items then just open this code and call api
-// app.post("/products", async (req, res) => {
-//   let data = await Item.insertMany([...aaa]);
-//   res.json(data);
-//   res.json(products);
-// });
-
+////////////////////////////////////// GET ALL PRODUCTS START /////////////////////
 app.get("/allProducts", async (req, res) => {
   let data = await Item.find({});
   res.json(data);
 });
+////////////////////////////////////// GET ALL PRODUCTS START /////////////////////
+
+///////////////////////////////////////////////// ADD COPOUN START/////////////////////
+const COPOUNSchema = new mongoose.Schema({
+  dealNumber: String,
+  description: String,
+  discount: String,
+  copounCode: String,
+  isAvailable: Boolean,
+});
+const Copouns = mongoose.model("copouns", COPOUNSchema);
+
+app.post("/send-copoun", async (req, res) => {
+  const updateCopounsDatabase = new Copouns(req.body);
+  updateCopounsDatabase.save();
+  // let data = await Copouns.insertMany([...productsData]);
+  // console.log(data);
+
+  // Get all the push tokens from the database
+  const pushTokens = await tokenPush.find({});
+
+  // Get a list of all valid push tokens
+  const validPushTokens = [];
+  for (let pushToken of pushTokens) {
+    if (Expo.isExpoPushToken(pushToken.pushToken)) {
+      validPushTokens.push(pushToken.pushToken);
+    }
+  }
+  console.log(validPushTokens);
+
+  // Divide the valid push tokens into chunks of 100
+  const chunks = expo.chunkPushNotifications(
+    validPushTokens.map((pushToken) => ({
+      to: pushToken,
+      sound: "default",
+      body: `${req.body.description}`,
+      data: req.body,
+    }))
+  );
+
+  // Send push notifications in chunks
+  for (let chunk of chunks) {
+    try {
+      const receipts = await expo.sendPushNotificationsAsync(chunk);
+      console.log(receipts);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  res.send({ success: true });
+});
+///////////////////////////////////////////////// ADD COPOUN END/////////////////////
+
+//////////////////////////////////// GET ALL COPOUNS START///////////////////
+app.get("/copouns", async (req, res) => {
+  const copouns = await Copouns.find({});
+  console.log(copouns);
+  res.send(copouns);
+});
+//////////////////////////////////// GET ALL COPOUNS END///////////////////
+
 app.listen(port, () => {
   console.log("listning....");
 });
